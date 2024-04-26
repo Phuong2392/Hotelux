88554f30b405250f920c082afa3b8a1919df259e
phuong <ntkphuong020392@gmail.com>
2024-04-26 21:44:52 +0700

    Create Hotelux Project

---
+++ Hotelux/src/main/java/hotelux/HoteluxApplication.java
@@ -0,0 +1,13 @@
+package hotelux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoteluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoteluxApplication.class, args);
+    }
+
+}
